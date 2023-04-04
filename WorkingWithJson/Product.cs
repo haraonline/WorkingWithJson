4224553862b2036a6fcb23d6b6791adaea88f615
Hara Gopal Kovvali <HaraGopal.Kovvali@ece.com>
2023-04-04 13:19:50 +0200

    refine

---
+++ WorkingWithJson/Product.cs
@@ -0,0 +1,23 @@
+﻿namespace WorkingWithJson
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string[] Colors { get; set; }
+
+        public Product(int id, string name, string description, string[] colors)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+            this.Colors = colors;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Description: {Description}, Colors: {string.Join(",", Colors)}";
+        }
+    }
+}
