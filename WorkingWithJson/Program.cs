3f5456ee3685365072bd9cb18bac82f6836f1cac
Hara Gopal Kovvali <HaraGopal.Kovvali@ece.com>
2023-04-04 13:21:58 +0200

    refine

--- WorkingWithJson/Program.cs
+++ WorkingWithJson/Program.cs
@@ -9,12 +9,12 @@ namespace WorkingWithJson
             //JsonConvert       : Wrapper over JsonSerializer. Use this for simple scenarios
             //JsonSerializer    : Use this for more control
 
-            //JsonConvert example: serialze (.NET obj to string)
+            //JsonConvert Example: Serialze (.NET Object to string)
             Product product1 = new Product(1, "iphone", "v.1", new string[] { "Red", "Green", "yellow" });
             string outJsonStr = JsonConvert.SerializeObject(product1);
             Console.WriteLine("Object to string: " + outJsonStr);
 
-            //JsonConvert example: deserialze (string to .NET object)
+            //JsonConvert Example: Deserialze (String to .NET Object)
             string inJsonStr = "{\"Id\":1,\"Name\":\"iphone\",\"Description\":\"v.2\",\"Colors\":[\"Purple\",\"Orange\",\"Brown\"]}";
             Product product2 = JsonConvert.DeserializeObject<Product>(inJsonStr);
             Console.WriteLine("String to Object: " + product2);
